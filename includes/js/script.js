64f3b39eef9cc8156404aa45dde1f64d1e1a90b7
unknown <trainee@PC328674.cts.com>
2016-07-15 16:36:37 +0530

    home completed

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,17 +1,29 @@
-/*
 
-My Custom JS
-============
+function showHide(divId){
+  if (divId == 2) {
+        var divId = 'CONTENT'+divId;
+        var theDiv = document.getElementById(divId);
+        if(theDiv.style.display=="block"){
+            theDiv.style.display="none";
+        }else{
+            theDiv.style.display="block";
+        }
+          document.getElementById("CONTENT1").style.display="none";
+          document.getElementById("jscolor2").style.color="#5f5743";
+          document.getElementById("jscolor1").style.color="white";
+    }
+    if (divId == 1) {
+          var divId = 'CONTENT'+divId;
+          var theDiv = document.getElementById(divId);
+          if(theDiv.style.display=="block"){
+              theDiv.style.display="none";
+          }else{
+              theDiv.style.display="block";
+          }
+          document.getElementById("CONTENT2").style.display="none";
+          document.getElementById("jscolor1").style.color="#5f5743";
+          document.getElementById("jscolor2").style.color="white";
 
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
 
-*/
-$(function() {
-  $('#alertMe').click(function(e) {
-    e.preventDefault();
-      $('#successAlert').slideDown();
-  });
-
-});
+      }
+  }
